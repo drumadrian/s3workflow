44c20518c6f14db71a3a60e9d4d58747f217e6f9
Adrian Drummond <adrian.drummond@gmail.com>
2021-03-02 07:34:00 -0800

    wip

--- cdk/cdk/cdk_stack.py
+++ cdk/cdk/cdk_stack.py
@@ -195,7 +195,7 @@ class CdkStack(core.Stack):
                                                             )
 
 
-        parseimagelistfile.add_environment("ELASTICSEARCH_HOST", s3workflow_pool.s3workflow_domain )
+        parseimagelistfile.add_environment("ELASTICSEARCH_HOST", s3workflow_domain.domain_endpoint )
         parseimagelistfile.add_environment("QUEUEURL", images_queue.queue_url )
         parseimagelistfile.add_environment("DEBUG", "False" )
 
