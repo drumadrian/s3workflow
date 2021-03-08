91de41ef043930a5a4cf5a36427a69f25f9c1832
Adrian Drummond <adrian.drummond@gmail.com>
2021-03-08 00:41:09 -0800

    adding environment variables for list-objects lambda function

--- cdk/cdk/cdk_stack.py
+++ cdk/cdk/cdk_stack.py
@@ -265,9 +265,16 @@ class CdkStack(core.Stack):
         parse_image_list_file.add_environment("BUCKET", "-" )
         parse_image_list_file.add_environment("KEY", "-" )
 
+        
+        list_objects.add_environment("QUEUEURL", object_queue.queue_url)
+        list_objects.add_environment("ELASTICSEARCH_HOST", s3workflow_domain.domain_endpoint)
+        list_objects.add_environment("S3_BUCKET_NAME", images_bucket.bucket_name)
+        list_objects.add_environment("S3_BUCKET_PREFIX", "images/")
+        list_objects.add_environment("S3_BUCKET_SUFFIX", "")
+        list_objects.add_environment("LOGGING_LEVEL", "INFO")
+
         get_size_and_store.add_environment("QUEUEURL", object_queue.queue_url)
         get_size_and_store.add_environment("ELASTICSEARCH_HOST", s3workflow_domain.domain_endpoint)
-        
         get_size_and_store.add_environment("S3_BUCKET_NAME", images_bucket.bucket_name)
         get_size_and_store.add_environment("S3_BUCKET_PREFIX", "images/")
         get_size_and_store.add_environment("S3_BUCKET_SUFFIX", "")
